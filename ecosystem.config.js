cb5b9a6da431d2fcf892d6be239e946f9d124d55
来凯涛 <>
2023-12-18 13:56:40 +0800

    fix: pm2 配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,13 @@
  * @Autor: laikt
  * @Date: 2023-12-18 11:21:46
  * @LastEditors: laikt
- * @LastEditTime: 2023-12-18 11:36:55
+ * @LastEditTime: 2023-12-18 13:53:54
  */
 module.exports = {
   apps: [
     {
       name: 'shop-app',
       script: './dist/apps/shop-app/main.js',
-      env: {
-        NODE_ENV: 'production',
-        JWT_SECRET: 'mysecret',
-      },
     },
   ],
 };
